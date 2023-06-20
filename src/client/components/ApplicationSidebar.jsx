87e3057ad3697a74bd7b7b990871364c3eeb3777
Scott Weaver <scott@weaverscott.com>
2023-06-20 15:59:57 -0500

    Label, Switch

--- src/client/components/ApplicationSidebar.jsx
+++ src/client/components/ApplicationSidebar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Button } from "./ui/Button";
-import { PlayCircleIcon, PencilIcon, ArmchairIcon } from "lucide-react";
+import {
+  PlayCircleIcon,
+  PencilIcon,
+  ArmchairIcon,
+  EraserIcon,
+} from "lucide-react";
 import { cn } from "../utils";
+import { Switch } from "./ui/Switch";
+import { Label } from "./ui/Label";
 
 function ApplicationSidebar({ className }) {
   return (
@@ -21,6 +28,10 @@ function ApplicationSidebar({ className }) {
               Walls
             </Button>
             <Button variant="ghost" size="sm" className="w-full justify-start">
+              <EraserIcon className="mr-2 h-4 w-4" />
+              Eraser
+            </Button>
+            <Button variant="ghost" size="sm" className="w-full justify-start">
               <ArmchairIcon className="mr-2 h-4 w-4" />
               Furniture
             </Button>
@@ -28,8 +39,24 @@ function ApplicationSidebar({ className }) {
         </div>
         <div className="py-2">
           <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-            Info
+            Tool Info:
           </h2>
+          <div className="px-4 py-2">
+            <div className="space-y-1">
+              <span className="block px-4 py-2">
+                Length: <span className="font-semibold">10m</span>
+              </span>
+              <span className="block px-4 py-2">
+                Height: <span className="font-semibold">10m</span>
+              </span>
+              <span className="block px-4 py-2">
+                <div className="flex items-center space-x-2">
+                  <Switch id="show-grid" />
+                  <Label htmlFor="show-grid">Show Grid</Label>
+                </div>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
