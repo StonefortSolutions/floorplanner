24fef544fbb2a15b436db2465c5264d4fc1cbabe
cdriesner <driesnercalvin@gmail.com>
2023-06-20 16:42:41 -0700

    added 2d 3d toggle

--- src/client/components/edditor3d/main.jsx
+++ src/client/components/edditor3d/main.jsx
@@ -5,13 +5,22 @@ import { OrbitControls } from '@react-three/drei';
 import Ground from './components/Ground'
 import Grid from './components/Grid';
 import SnapPoints from './components/SnapPoints';
+import * as THREE from 'three'
 
 const Edditor3d = () => {
   const [is2D, setIs2D] = useState(true)
-  //const [selectedPoint, setSelectedPoint] =
+  const camera2D = new THREE.OrthographicCamera( 10 / - 2, 10 / 2, 10 / 2, 10 / - 2, 1, 1000 )
+  camera2D.translateY(50);
+  const camera3D =new THREE.PerspectiveCamera(75, 50 / 50, 0.1, 1000)
+  camera3D.translateY(50);
+  camera3D.translateX(50);
   return (
     <div id="edditor" className='w-[1000px] h-[600px]'>
-      <Canvas id="canvas1"camera={{position: [0,20, 20]}}>
+      <button onClick={()=>(setIs2D(!is2D))}>toggle perspective</button>
+      <Canvas 
+        id="canvas1"
+        camera={is2D ? camera2D : camera3D}
+      >
         <OrbitControls enableZoom={true} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[-2, 10, 2]} intensity={1} />
