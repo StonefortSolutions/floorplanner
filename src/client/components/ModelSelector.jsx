0037fb98d05e0f02fd0d26e1ae519c5b29ea37f6
cdriesner <driesnercalvin@gmail.com>
2023-06-22 13:58:49 -0700

    added pictures for item select

--- src/client/components/ModelSelector.jsx
+++ src/client/components/ModelSelector.jsx
@@ -1,18 +1,43 @@
 import { furnitureNames } from "./data/furnitureName"
 import {useDispatch} from 'react-redux'
 import { setModel } from "../store/selectedModel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/Card"
+import { Button } from "./ui/Button"
 
 const ModelSelector = () => {
   const dispatch = useDispatch();
   console.log(furnitureNames)
   return(
-    <div className="w-48 h-48 overflow-y-scroll">
-      {furnitureNames.map((name)=>(
-        <button onClick={() => dispatch(setModel(name))}>
-          {name}
-        </button>
-      ))}
+    <div>
+      <h1 className="mb-2 px-2 text-lg font-semibold tracking-tight">furniture</h1>
+      <div className="overflow-y-scroll overflow-x-hidden flex flex-row flex-wrap lg:w-[500px] lg:h-[700px]">
+        {furnitureNames.map((name)=>(
+          <Card className="w-52 m-2">
+            <CardHeader>
+              <CardDescription>
+                {name}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <img src={`/ItemPictures/${name}_SE.png`}/>
+            </CardContent>
+            <CardFooter>
+              <Button variant="outline" onClick={() => dispatch(setModel(name))}>
+                Select
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
+    
   )
 }
 
--- src/client/pages/Editor.jsx
+++ src/client/pages/Editor.jsx
@@ -7,10 +7,10 @@ function Editor() {
   return (
     <div className="flex flex-col lg:flex-row p-2 h-full w-full">
       <ApplicationSidebar className="border-r flex" />
-      <ModelSelector/>
       <div className="min-w-[200px] min-h-[200px] flex-1 bg-secondary border p-2">
         <Editor3d />
       </div>
+      <ModelSelector className="border-r flex"/>
     </div>
   );
 }
