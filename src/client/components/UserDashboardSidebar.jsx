bf898fa04f6533a9e343ec262e18cac49fa368ca
Scott Weaver <scott@weaverscott.com>
2023-06-26 14:54:58 -0500

    dedupe

--- src/client/components/UserDashboardSidebar.jsx
+++ src/client/components/UserDashboardSidebar.jsx
@@ -13,10 +13,91 @@ import { Button } from "./ui/Button";
 import { Popover, PopoverTrigger, PopoverContent } from "./ui/Popover";
 import { cn } from "../utils";
 
-function UserDashboardSidebar({ className }) {
+function SidebarContent() {
   const navigate = useNavigate();
   const location = useLocation();
+  return (
+    <>
+      <div className="space-y-4 py-4">
+        <div className="px-4 py-2">
+          <a href="/editor">
+            <Button
+              variant="default"
+              size="sm"
+              className="w-full justify-start"
+            >
+              <PlusCircleIcon className="mr-2 h-4 w-4" />
+              Create New Floorplan
+            </Button>
+          </a>
+        </div>
+      </div>
+      <div className="py-4 space-y-4">
+        <h2 className="relative px-6 text-lg font-semibold tracking-tight">
+          Featured Templates
+        </h2>
+      </div>
+      <div className="space-y-4 py-4">
+        <div className="px-4 py-2">
+          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
+            My Floorplans
+          </h2>
+          <div className="space-y-1">
+            <Button
+              variant={
+                location.pathname === "/dashboard" ? "secondary" : "ghost"
+              }
+              size="sm"
+              className="w-full justify-start"
+              onClick={() => navigate(`/dashboard`)}
+            >
+              <Clock10Icon className="mr-2 h-4 w-4" />
+              Recent
+            </Button>
+            <Button
+              variant={
+                location.pathname === "/dashboard/favorites"
+                  ? "secondary"
+                  : "ghost"
+              }
+              size="sm"
+              className="w-full justify-start"
+              onClick={() => navigate(`/dashboard/favorites`)}
+            >
+              <StarIcon className="mr-2 h-4 w-4" />
+              Favorites
+            </Button>
+          </div>
+        </div>
 
+        <div className="py-2">
+          <h2 className="relative px-6 text-lg font-semibold tracking-tight">
+            My Subscription
+          </h2>
+          <div className="px-4 py-2">
+            <div className="space-y-1">
+              <Button
+                variant={
+                  location.pathname === "/dashboard/pricing"
+                    ? "secondary"
+                    : "ghost"
+                }
+                size="sm"
+                className="w-full justify-start"
+                onClick={() => navigate(`/dashboard/pricing`)}
+              >
+                <CreditCardIcon className="mr-2 h-4 w-4" />
+                Subscription
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function UserDashboardSidebar({ className }) {
   return (
     <>
       <div className={cn("pb-12 md:hidden", className)}>
@@ -31,160 +112,12 @@ function UserDashboardSidebar({ className }) {
             </Button>
           </PopoverTrigger>
           <PopoverContent align="center">
-            <div className="space-y-4 py-4">
-              <div className="px-4 py-2">
-                <a href="/editor">
-                  <Button
-                    variant="default"
-                    size="sm"
-                    className="w-full justify-start"
-                  >
-                    <PlusCircleIcon className="mr-2 h-4 w-4" />
-                    Create New Floorplan
-                  </Button>
-                </a>
-              </div>
-            </div>
-            <div className="py-4 space-y-4">
-              <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-                Featured Templates
-              </h2>
-            </div>
-            <div className="space-y-4 py-4">
-              <div className="px-4 py-2">
-                <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-                  My Floorplans
-                </h2>
-                <div className="space-y-1">
-                  <Button
-                    variant={
-                      location.pathname === "/dashboard" ? "secondary" : "ghost"
-                    }
-                    size="sm"
-                    className="w-full justify-start"
-                    onClick={() => navigate(`/dashboard`)}
-                  >
-                    <Clock10Icon className="mr-2 h-4 w-4" />
-                    Recent
-                  </Button>
-                  <Button
-                    variant={
-                      location.pathname === "/dashboard/favorites"
-                        ? "secondary"
-                        : "ghost"
-                    }
-                    size="sm"
-                    className="w-full justify-start"
-                    onClick={() => navigate(`/dashboard/favorites`)}
-                  >
-                    <StarIcon className="mr-2 h-4 w-4" />
-                    Favorites
-                  </Button>
-                </div>
-              </div>
-
-              <div className="py-2">
-                <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-                  My Subscription
-                </h2>
-                <div className="px-4 py-2">
-                  <div className="space-y-1">
-                    <Button
-                      variant={
-                        location.pathname === "/dashboard/pricing"
-                          ? "secondary"
-                          : "ghost"
-                      }
-                      size="sm"
-                      className="w-full justify-start"
-                      onClick={() => navigate(`/dashboard/pricing`)}
-                    >
-                      <CreditCardIcon className="mr-2 h-4 w-4" />
-                      Subscription
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <SidebarContent className="w-full" />
           </PopoverContent>
         </Popover>
       </div>
       <div className={cn("pb-12 hidden md:block", className)}>
-        <div className="space-y-4 py-4">
-          <div className="px-4 py-2">
-            <a href="/editor">
-              <Button
-                variant="default"
-                size="sm"
-                className="w-full justify-start"
-              >
-                <PlusCircleIcon className="mr-2 h-4 w-4" />
-                Create New Floorplan
-              </Button>
-            </a>
-          </div>
-        </div>
-        <div className="py-4 space-y-4 border-t">
-          <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-            Featured Templates
-          </h2>
-        </div>
-        <div className="space-y-4 py-4 border-t">
-          <div className="px-4 py-2">
-            <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-              My Floorplans
-            </h2>
-            <div className="space-y-1">
-              <Button
-                variant={
-                  location.pathname === "/dashboard" ? "secondary" : "ghost"
-                }
-                size="sm"
-                className="w-full justify-start"
-                onClick={() => navigate(`/dashboard`)}
-              >
-                <Clock10Icon className="mr-2 h-4 w-4" />
-                Recent
-              </Button>
-              <Button
-                variant={
-                  location.pathname === "/dashboard/favorites"
-                    ? "secondary"
-                    : "ghost"
-                }
-                size="sm"
-                className="w-full justify-start"
-                onClick={() => navigate(`/dashboard/favorites`)}
-              >
-                <StarIcon className="mr-2 h-4 w-4" />
-                Favorites
-              </Button>
-            </div>
-          </div>
-
-          <div className="py-2 border-t">
-            <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-              My Subscription
-            </h2>
-            <div className="px-4 py-2">
-              <div className="space-y-1">
-                <Button
-                  variant={
-                    location.pathname === "/dashboard/pricing"
-                      ? "secondary"
-                      : "ghost"
-                  }
-                  size="sm"
-                  className="w-full justify-start"
-                  onClick={() => navigate(`/dashboard/pricing`)}
-                >
-                  <CreditCardIcon className="mr-2 h-4 w-4" />
-                  Subscription
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SidebarContent />
       </div>
     </>
   );
