4961bcdf8cbbaf1824a967192cea1e28aa0e97d5
Julian Bombard <jpbombard@yahoo.com>
2023-06-27 12:42:05 -0400

    basic floorplan thunks

---
+++ src/client/store/floorplan.jsx
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  floorplans: Array(0),
+  singleFloorplan: {},
+};
+
+export const fetchFloorplans = createAsyncThunk("fetchFloorplans", async () => {
+  try {
+    const { data } = await axios.get("/api/floorplan");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const fetchSingleFloorplan = createAsyncThunk(
+  "fetchSingleFloorplan",
+  async (id) => {
+    try {
+      const { data } = await axios.get(`/api/floorplan/${id}`);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const floorplanSlice = createSlice({
+  name: "floorplan",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchFloorplans.fulfilled, (state, action) => {
+      state.floorplans = action.payload;
+    });
+    builder.addCase(fetchSingleFloorplan.fulfilled, (state, action) => {
+      state.singleFloorplan = action.payload;
+    });
+  },
+});
+
+export default floorplanSlice.reducer;
