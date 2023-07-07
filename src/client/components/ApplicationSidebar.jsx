4b089600e70c1f8a23852fa5c3007e2be94ebe79
cdriesner <driesnercalvin@gmail.com>
2023-07-07 14:30:41 -0700

    sortof got the camera to stay in place

--- src/client/components/ApplicationSidebar.jsx
+++ src/client/components/ApplicationSidebar.jsx
@@ -80,6 +80,15 @@ export function ApplicationButtons({ className }) {
         Eraser
       </Button>
       <Button
+        variant={currentAction === "edit" ? "secondary" : "ghost"}
+        size="sm"
+        className={currentAction === "edit" ? "w-full justify-start bg-gradient-to-br from-red-600 to-orange-500" : "w-full justify-start hover:scale-105"}
+        onClick={() => dispatch(setAction("edit"))}
+      >
+        <EraserIcon className="mr-2 h-4 w-4" />
+        Edit !!! In Progress !!!
+      </Button>
+      <Button
         variant={currentAction === "placeItem" ? "secondary" : "ghost"}
         size="sm"
         className={currentAction === "placeItem" ? "w-full justify-start bg-gradient-to-br from-red-600 to-orange-500" : "w-full justify-start hover:scale-105"}
@@ -96,6 +105,15 @@ export function ApplicationButtons({ className }) {
       >
         <DoorClosed className="mr-2 h-4 w-4" />
         Door
+      </Button>
+      <Button
+        variant={currentAction === "firstPerson" ? "secondary" : "ghost"}
+        size="sm"
+        className={currentAction === "firstPerson" ? "w-full justify-start bg-gradient-to-br from-red-600 to-orange-500" : "w-full justify-start hover:scale-105"}
+        onClick={() => dispatch(setAction("firstPerson"))}
+      >
+        <DoorClosed className="mr-2 h-4 w-4" />
+        First Person !Dont click!
       </Button>
       <Button
         onClick={() => dispatch(saveFloorplan(singleFloorplan))}
--- src/client/components/edditor3d/components/Controls.jsx
+++ src/client/components/edditor3d/components/Controls.jsx
@@ -1,9 +1,16 @@
 import { OrbitControls } from "@react-three/drei";
 import {useSelector} from 'react-redux'
 import * as THREE from "three";
-const Controls = ({is2D}) => {
-  return (
+import {useState, useEffect} from "react"
+const Controls = ({is2D, camera}) => {
+  const [transform, setTransform] = useState({position: null, rotation: null, quaternion: null})
+  const canvas = document.getElementById('canvas1')
+  const currentAction = useSelector(state => state.currentAction)
+  const controls = (
     <OrbitControls 
+      domElement={canvas}
+      camera={camera}
+      target={[0,0,0]}
       enableRotate={!is2D} 
       mouseButtons={{
         LEFT: null,
@@ -14,8 +21,23 @@ const Controls = ({is2D}) => {
       maxPolarAngle={Math.PI /2.1}
       minZoom={1}
       maxZoom={100}
+      onChange={() => {change()}}
     />
   )
+
+  const change = () => {
+    setTransform({position:controls.props.camera.position,rotation:controls.props.camera.rotation,quaternion:controls.props.camera.quaternion})
+  }
+
+  useEffect(()=>{
+    if(transform.position !== null){
+      controls.props.camera.position.set(transform.position.x,transform.position.y,transform.position.z)
+      controls.props.camera.rotation.set(transform.rotation.x,transform.rotation.y,transform.rotation.z,"XYZ")
+      controls.props.camera.quaternion.set(transform.quaternion.x,transform.quaternion.y,transform.quaternion.z,transform.quaternion.w)
+    }
+  },[currentAction])
+
+  return controls
 }
 
 export default Controls
