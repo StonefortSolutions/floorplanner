2368259f3469c95ba4f1d4a393e21beeba2bc1da
cdriesner <driesnercalvin@gmail.com>
2023-07-10 12:24:16 -0700

    got rid of normals

--- src/client/components/edditor3d/components/Ground.jsx
+++ src/client/components/edditor3d/components/Ground.jsx
@@ -9,14 +9,10 @@ import { useLoader } from '@react-three/fiber';
  */
 const Ground = ({ size }) => {
   const mesh = useRef();
-  const normals = useLoader(THREE.TextureLoader, "/normals/groundNormal.png");
-  normals.wrapS = THREE.RepeatWrapping
-  normals.wrapT = THREE.RepeatWrapping
-  normals.repeat.set(8,8)
   return (
     <mesh ref={mesh} position ={[0,-1.5,0]}name='ground'>
       <boxGeometry args={[size, 4,size]} />
-      <meshStandardMaterial color={'#4f4945'} normalMap={normals}/>
+      <meshStandardMaterial color={'#4f4945'}/>
       {/* <meshStandardMaterial color={0x77ff00} /> */}
     </mesh>
   );
