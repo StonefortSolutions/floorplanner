9339eb12e9c4c925a2ee7da108e756a63b180eb0
Scott Weaver <scott@weaverscott.com>
2023-07-18 10:39:32 -0500

    scrollbar

--- src/client/pages/Layout.jsx
+++ src/client/pages/Layout.jsx
@@ -6,7 +6,7 @@ import { Toaster } from "../components/ui/Toaster";
 function Layout() {
   return (
     <div className="flex flex-col h-[98vh]">
-      <Navbar className="flex" />
+      <Navbar />
       <div className="mt-24 flex-1">
         <Outlet />
         <Toaster />
